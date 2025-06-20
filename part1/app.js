2481600f48bf8d0c62e9b5e2d52fee48316984c8
Grace Baek <a1904965@adelaide.edu.au>
2025-06-20 04:34:28 +0000

    [automatic commit] 2025-06-20 04:34:28 UTC 130.220.243.103

--- part1/app.js
+++ part1/app.js
@@ -189,7 +189,7 @@ app.get('/api/walkers/summary', async (req, res) => {
                 COUNT(DISTINCT wr.request_id) AS completed_walks
             FROM Users u
             LEFT JOIN WalkRatings r ON u.user_id = r.walker_id
-            LEFT JOIN WalkRequests wr ON wr.
+            LEFT JOIN WalkRequests wr ON wr.request_id = r.request_id AND wr.status = 'completed'
 
             `);
         res.json(rows);
