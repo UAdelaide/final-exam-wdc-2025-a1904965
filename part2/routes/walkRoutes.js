f9b51bfbab83bd32cd5bd88e1bb5aa2bad32b17e
Grace Baek <a1904965@adelaide.edu.au>
2025-06-20 15:52:34 +0000

    [automatic commit] 2025-06-20 15:52:34 UTC 115.131.116.34

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -38,7 +38,8 @@ router.post('/', async (req, res) => {
 // POST an application to walk a dog (from walker)
 router.post('/:id/apply', async (req, res) => {
   const requestId = req.params.id;
-  const { walker_id } = req.body;
+  const { walker_id } = req.session.user?.user_id;
+  
 
   try {
     await db.query(`
