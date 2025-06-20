d5e8d129e57761ce4dc0d2442cbd2b5a4c16c428
Grace Baek <a1904965@adelaide.edu.au>
2025-06-20 04:45:31 +0000

    [automatic commit] 2025-06-20 04:45:31 UTC 130.220.243.103

--- part1/app.js
+++ part1/app.js
@@ -137,15 +137,11 @@ let db;
 
         await db.execute(`
             INSERT INTO WalkApplications (request_id, walker_id, applied_at, status) VALUES
-            (6, (SELCT user_id FROM Users WHERE username = 'bobwalker'), '2025-6-13 08:00:00')`);
+            (6, (SELCT user_id FROM Users WHERE username = 'bobwalker'), '2025-6-13 08:00:00', 'accepted')
+            `);
 
             await db.execute(`
-            INSERT INTO Dogs (owner_id, name, size) VALUES
-            ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
-                ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small'),
-                ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Bear', 'large'),
-                ((SELECT user_id FROM Users WHERE username = 'grace123'), 'Winnie', 'medium'),
-                ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Levi', 'medium')`);
+            INSERT INTO `);
         }
     } catch (err) {
         console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
