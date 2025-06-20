651a1204ce746765e706cff254bdad88fd91441f
Grace Baek <a1904965@adelaide.edu.au>
2025-06-20 04:28:49 +0000

    [automatic commit] 2025-06-20 04:28:49 UTC 130.220.243.103

--- part1/app.js
+++ part1/app.js
@@ -159,9 +159,8 @@ app.get('/api/dogs', async (req, res) => {
         const [rows] = await db.execute('
             SELECT d.name AS dog_name, d.size, u.username AS owner_username
             FROM Dogs d
-            JOIN Users u ON d.owner_id = u.user_id
-            ORDER BY d.name');
-        res.json(books);
+            JOIN Users u ON d.owner_id = u.user_id');
+        res.json(rows);
     } catch (err) {
         res.status(500).json({ error: 'Failed to fetch books' });
     }
