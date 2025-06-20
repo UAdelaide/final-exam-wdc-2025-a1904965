c1b35992c8dfdd3c4fe3f99f7e9aeb4170114916
Grace Baek <a1904965@adelaide.edu.au>
2025-06-20 12:39:23 +0000

    [automatic commit] 2025-06-20 12:39:23 UTC 130.220.243.103

--- part2/server.js
+++ part2/server.js
@@ -5,7 +5,7 @@ const app = require('./app');
 // set port from environment variable OR default to 8080
 const PORT = process.env.PORT || 8080;
 
-// this starts the Express server and listens on specified port
+// this starts the Express server and listens on specified port for incoming requests
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
