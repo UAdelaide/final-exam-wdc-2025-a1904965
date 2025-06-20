2d460399823259b137579b13861baa119cd30fcd
Grace Baek <a1904965@adelaide.edu.au>
2025-06-20 04:51:48 +0000

    [automatic commit] 2025-06-20 04:51:47 UTC 130.220.243.103

--- part1/app.js
+++ part1/app.js
@@ -143,8 +143,10 @@ let db;
 
             await db.execute(`
             INSERT INTO WalkRatings (request_id, walker_id, owner_id, rating, comments, rated_at) VALUES
-                (6, (SELECT user_id FROM Users WHERE username = 'bobwalker), (SELECT user_id FROM Users WHERE username = 'alice123'),
+                (6, (SELECT user_id FROM Users WHERE username = 'bobwalker),
+                (SELECT user_id FROM Users WHERE username = 'alice123'),
                 5, 'Excellent Service', '2025-06-10 10:00:00')`);
+                ()
             ()
         }
     } catch (err) {
