ee4bf4b9d57f32f66a4c45d1ca901372df731191
Grace Baek <a1904965@adelaide.edu.au>
2025-06-20 04:33:05 +0000

    [automatic commit] 2025-06-20 04:33:05 UTC 130.220.243.103

--- part1/app.js
+++ part1/app.js
@@ -183,7 +183,8 @@ app.get('/api/walkers/summary', async (req, res) => {
     try {
         const [rows] = await db.execute(`
             SELECT u.username AS walker_username,
-            
+            COUNT(r.rating_id) AS total_ratings,
+            ROUND(AVG)
             `);
         res.json(rows);
     } catch (err) {
