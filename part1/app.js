c09eb058aba0e9eaa336d05c3cb0ba2310ddb1e3
Grace Baek <a1904965@adelaide.edu.au>
2025-06-20 04:21:29 +0000

    [automatic commit] 2025-06-20 04:21:29 UTC 130.220.243.103

--- part1/app.js
+++ part1/app.js
@@ -136,7 +136,9 @@ if (users[0].count === 0) {
 '2025-06-12 16:00:00', 30, 'Montrose Ave', 'open'),
 
 ((SELECT d.dog_id FROM Dogs d JOIN Users u ON d.owner_id = u.user_id WHERE d.name = 'Levi' AND u.username = 'carol123'),
-'2025-06-13 08:30:00', 45, 'Adelaide University', 'completed')`)
+'2025-06-13 08:30:00', 45, 'Adelaide University', 'completed')`);
+
+
 }
 
 
