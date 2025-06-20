fb6e0e48871b7a4e5af6edfefcab864f4925e656
Grace Baek <a1904965@adelaide.edu.au>
2025-06-20 04:53:51 +0000

    [automatic commit] 2025-06-20 04:53:51 UTC 130.220.243.103

--- part1/app.js
+++ part1/app.js
@@ -148,7 +148,7 @@ let db;
                 5, 'Excellent Service', '2025-06-10 10:00:00')`),
             (9, (SELECT user_id FROM Users WHERE username = 'christinewalker'),
                 (SELECT user_id FROM Users WHERE username = 'grace123'),
-                5, 'Excellent Service', '2025-06-12 18:00:00')
+                4, 'Excellent Service', '2025-06-12 18:00:00')
                 `);
         }
     } catch (err) {
