0da6242ed2b7f2eba06090850152df2a685b2401
Grace Baek <a1904965@adelaide.edu.au>
2025-06-20 23:41:14 +0000

    [automatic commit] 2025-06-20 23:41:13 UTC 115.131.116.34

--- part2/app.js
+++ part2/app.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
-const dogRoutes = require('/')
+const dogRoutes = require('../part1/routes/dogs'); 
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
