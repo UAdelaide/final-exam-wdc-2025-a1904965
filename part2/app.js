f104e6529db8db6dbb83fd51e5fc320c1176d158
Grace Baek <a1904965@adelaide.edu.au>
2025-06-20 17:38:20 +0000

    [automatic commit] 2025-06-20 17:38:20 UTC 115.131.116.34

--- part2/app.js
+++ part2/app.js
@@ -28,5 +28,7 @@ const userRoutes = require('./routes/userRoutes');
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
+// add 
+
 // Export the app instead of listening here
 module.exports = app;
