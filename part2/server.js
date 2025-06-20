2425c72da1e71411250617e64466721eb379e092
Grace Baek <a1904965@adelaide.edu.au>
2025-06-20 11:51:32 +0000

    [automatic commit] 2025-06-20 11:51:31 UTC 130.220.243.103

--- part2/server.js
+++ part2/server.js
@@ -5,5 +5,6 @@ const app = require('/app');
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-    console.log('Server running on http://localhost:${PORT}');
+    console.log(`Server running on http://localhost:${PORT}`);
 });
+
