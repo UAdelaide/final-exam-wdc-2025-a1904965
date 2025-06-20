4e8b4b273695afc889d470779ce132a09c454cce
Grace Baek <a1904965@adelaide.edu.au>
2025-06-20 04:07:01 +0000

    [automatic commit] 2025-06-20 04:07:00 UTC 130.220.243.103

--- part1/app.js
+++ part1/app.js
@@ -48,7 +48,17 @@ await db.execute(`
     role ENUM('owner', 'walker') NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )
-    `)
+    `);
+
+await db.execute(`
+    CREATE TABLE Dogs (
+    dog_id INT AUTO_INCREMENT PRIMARY KEY,
+    owner_id INT NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    size ENUM('small', 'medium', 'large') NOT NULL,
+    FOREIGN KEY (owner_id) REFERENCES Users(user_id)
+)
+    `);
 
 
 
