fa4c7711c876ddb6f40a8017d84fec2d5c601a68
Grace Baek <a1904965@adelaide.edu.au>
2025-06-20 04:07:21 +0000

    [automatic commit] 2025-06-20 04:07:21 UTC 130.220.243.103

--- part1/app.js
+++ part1/app.js
@@ -60,6 +60,19 @@ await db.execute(`
 )
     `);
 
+await db.execute(`
+    CREATE TABLE WalkRequests (
+    request_id INT AUTO_INCREMENT PRIMARY KEY,
+    dog_id INT NOT NULL,
+    requested_time DATETIME NOT NULL,
+    duration_minutes INT NOT NULL,
+    location VARCHAR(255) NOT NULL,
+    status ENUM('open', 'accepted', 'completed', 'cancelled') DEFAULT 'open',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (dog_id) REFERENCES Dogs(dog_id)
+)
+    `)
+
 
 
 
