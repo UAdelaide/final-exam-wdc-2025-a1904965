2207eae767937a6280e3e56d0ab580f7e527393b
Grace Baek <a1904965@adelaide.edu.au>
2025-06-20 13:03:42 +0000

    [automatic commit] 2025-06-20 13:03:42 UTC 130.220.243.103

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -70,8 +70,10 @@ router.post('/logout', async (req, res) => {
   req.session.destroy((err) => {
     if (err) {
       console.error('Session destruction error', err);
-      
+      return res.status(500).json({ error: 'Failed to logout' });
     }
+
+    
   })
   const { username, password } = req.body;
 
