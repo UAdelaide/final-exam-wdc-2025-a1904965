4b7def7d8f79d1c1b9b13e1fafd4ee8c74edebec
Grace Baek <a1904965@adelaide.edu.au>
2025-06-21 00:30:22 +0000

    [automatic commit] 2025-06-21 00:30:22 UTC 115.131.116.34

--- part2/app.js
+++ part2/app.js
@@ -25,6 +25,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
 
+const mysql = require('mysql2/promise');
 db = await mysql.createConnection({
     host: 'localhost',
     user: 'root',
